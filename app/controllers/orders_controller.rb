50e551ca9dd3004e29a7cf74928e0f789057c15e
sammarcus <sam.marcus@me.com>
2015-05-20 14:30:00 +0100

    order controller work (create)

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,21 +1,51 @@
 class OrdersController < ApplicationController
   require 'mandrill'
 
+  def index
+    @order = Order.all
+  end
+
   def new
+    @order = Order.new
   end
 
   def create
-    # # Create the order from params
-    # @customer = customer.new(params[:customer])
-    # if @customer.save
-    #   # Delivers new order email possibly?
-    #   emailer.order_email(@customer).deliver
-    #   redirect_to(@customer, :notice => 'New Order')
-    # else
-    #   render :action => 'new'
-    # end
-
+    @order = Order.new(order_params)
+    if @order.save
+      redirect_to @order
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @order = Order.find(params[:id])
+  end
+
+  private
+  def order_params
+    params.require(:order).permit(:name)
+  end
+
+
+
+
+
+# bring out params
+# render plain: params[:order].inspect
+
+# Mailer
+#     # Create the order from params
+#     @customer = customer.new(params[:customer])
+#     if @customer.save
+#       # Delivers new order email possibly?
+#       emailer.order_email(@customer).deliver
+#       redirect_to(@customer, :notice => 'New Order')
+#     else
+#       render :action => 'new'
+#     end
+
+
 
 
 # m = Mandrill::API.new
