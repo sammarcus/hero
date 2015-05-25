5389ed4a367cfa7cca07dc4830bb753647395fde
sammarcus <sam.marcus@me.com>
2015-05-25 14:30:00 +0100

    changed rails admin  pref

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -5,12 +5,12 @@ RailsAdmin.config do |config|
     dashboard                     # mandatory
     index                         # mandatory
     new
-    export
+    # export
     bulk_delete
     show
     edit
     delete
-    show_in_app
+    # show_in_app
 
     ## With an audit adapter, you can add:
     # history_index
