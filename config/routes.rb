b26708e6b7cc3ce30ac2f10f4b69ec2c9e138096
sammarcus <sam.marcus@me.com>
2015-05-25 14:30:00 +0100

    routes: added resources routes

--- config/routes.rb
+++ config/routes.rb
@@ -11,8 +11,15 @@ Rails.application.routes.draw do
 
   resources :orders
   # root to: 'orders#new'
+
   resources :flavors
   # root to: 'flavors#new'
+
+  resources :toppings
+  # root to: 'toppings#new'
+
+  resources :containments
+  # root to: 'containments#new'
 
   root 'home#index'
   get 'home/index'
